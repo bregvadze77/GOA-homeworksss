121702d1c6ed59f36b9055a06f0e31b9793c1929
bregvadze77 <guribregvadze00@gmail.com>
2024-05-25 23:24:54 +0400

    second commit

---
+++ day 2/day2.py
@@ -0,0 +1,29 @@
+name = "guro"
+surname = "bregvadze"
+age = 16
+height = 1.65
+is_boxer = True 
+is_learner = True 
+is_loveliboy = True
+hehavebike = True 
+hehavetwobrather = True
+heisathlate = True 
+print("my name is" + name + surname + "my height is" + str(height))
+
+
+num1 = 10
+num2 = 20
+num3 = 35
+
+print(num1 + num2)
+print(num3 - num2)
+print(num2 / num1)
+print(num2 // num1)
+print(num3 * num1)
+print(num3 % num2)
+print(num2 ** num3)
+print(num3 + num2)
+print(num1 + num2)
+print(num2 + num2)
+
+
