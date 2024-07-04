64222fea08aedefb0b59653c6b1718b88cb3fe76
bregvadze77 <guribregvadze00@gmail.com>
2024-07-04 18:25:51 +0400

    second commit

---
+++ day7/homework.py
@@ -0,0 +1,22 @@
+print(5+3) #true
+print(5>3) #true
+print(5>=3) #true
+print(5-3!=3)#true
+print(5-3==2)#true
+print(3<5)#true
+print(3<=5)#true
+
+
+num1=int(input())
+num2=int(input())
+
+print(num1 > num2)
+rint(num1 < num2)
+rint(num1  == num2)
+rint(num1 != num2)
+rint(num1 <= num2)
+rint(num1 >= num2)
+
+number1 = 6
+result = number1 <8
+print(result)
