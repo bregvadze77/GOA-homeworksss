82e46497d207ed907259df6825a82c8b8e3821f3
bregvadze77 <guribregvadze00@gmail.com>
2024-09-04 22:52:46 +0400

    second commit

---
+++ day14/main.py
@@ -0,0 +1,32 @@
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4]) 
+print(list[5])
+print(list[6])
+print(list[7])
+print(list[8])
+print(list[9])
+print(list[10])
+print(list[11])
+print(list[12])
+print(list[13])
+print(list[14])
+
+print(list[0:4])
+print(list[5:12])
+print(list[7:13])
+print(list[2:11])
+print(list[5:12])
+
+list[3] = 12 
+list[6] = 11
+list[8] = 14
+list[2] = 10
+
+list[1:3] = 20,30
+
+list[3:5] = 50,70
