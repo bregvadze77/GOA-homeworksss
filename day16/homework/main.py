31198722e2bb2fa487f54b796ed90bc1b03c4d7e
bregvadze77 <guribregvadze00@gmail.com>
2024-10-03 18:25:26 +0400

    second commit

---
+++ day16/homework/main.py
@@ -0,0 +1,10 @@
+def print_range(start,end):
+    for i in range(start,end + 1):
+        print(i)
+
+def info(name,surname,age,academi):
+    print("my name is" + name + surname + "im" + age "i studi in" "academi")
+
+def person(name,surname):
+    print(name,surname)
+    
